bfbba864c946ce6fdf6f79e7bd9e41af448c586d
sirrotich <rotichtitus12@gmail.com>
2019-05-26 17:41:19 +0300

    class for posts

--- posts/models.py
+++ posts/models.py
@@ -26,3 +26,36 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user = id).first()
         return profile
+class Image(models.Model):
+    # image_pic = models.ImageField(upload_to = 'p/', default='Image')
+    photo = ImageField(blank=True, manual_crop='800x800')
+    image_name = models.CharField(max_length = 50)
+    image_caption = HTMLField(blank=True)
+    post_date = models.DateTimeField(auto_now=True)
+    likes = models.BooleanField(default=False)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('-post_date',)
+
+    def save_image(self):
+        self.save()
+    
+    @classmethod
+    def update_caption(cls, update):
+        pass
+    
+    @classmethod
+    def get_image_id(cls, id):
+        image = Image.objects.get(pk=id)
+        return image
+    
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk = profile)
+        return images
+    
+    @classmethod
+    def get_all_images(cls):
+        images = Image.objects.all()
+        return images
