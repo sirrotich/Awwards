0a97b5e24b19524d5aa6d7538094c44961339ee2
sirrotich <rotichtitus12@gmail.com>
2019-05-26 18:10:45 +0300

    set pyuploadcare in settings file

--- awards/settings.py
+++ awards/settings.py
@@ -35,6 +35,7 @@ UPLOADCARE = {
 # Application definition
 
 INSTALLED_APPS = [
+    'pyuploadcare.dj',
     'posts.apps.PostsConfig',
     'bootstrap4',
     'tinymce',
@@ -125,3 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
