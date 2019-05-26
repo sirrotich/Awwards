a8050045b1ae083c43cf1be362b59b3bf54f7c6b
sirrotich <rotichtitus12@gmail.com>
2019-05-26 17:24:00 +0300

    create user profile page

--- posts/views.py
+++ posts/views.py
@@ -20,3 +20,30 @@ def signup(request):
         else:
             form = SignupForm()
             return render(request, 'registration/signup.html',{'form':form})
+
+def profile(request, username):
+    profile = User.objects.get(username=username)
+    # print(profile.id)
+    try:
+        profile_details = Profile.get_by_id(profile.id)
+    except:
+        profile_details = Profile.filter_by_id(profile.id)
+    images = Image.get_profile_images(profile.id)
+    title = f'@{profile.username} posts'
+
+    return render(request, 'profile/profile.html', {'title':title, 'profile':profile, 'profile_details':profile_details, 'images':images})
+
+# @login_required(login_url='/accounts/login')
+def upload_image(request):
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            upload = form.save(commit=False)
+            upload.profile = request.user
+            # print(f'image is {upload.image}')
+            upload.save()
+            return redirect('profile', username=request.user)
+    else:
+        form = ImageForm()
+    
+    return render(request, 'profile/upload_image.html', {'form':form})
